d973961d4f848b1f03ad53198561d07a17ccc2b5
Michelle Hey <michellemhey@gmail.com>
2013-03-12 16:08:36 -0500

    Second Commit

--- js/external.js
+++ js/external.js
@@ -1,47 +1,99 @@
-
 $(document).ready(function() {
-	//function myPost() {
+
+
+  // Posts users comment to server
 	$("#postComment").click(function() {
-  console.log("whatever!");
+    $.ajax({
+        type: "POST",
+        url: "/backliftapp/userCommentBelow",
+        data: {
+        userName: $("#userName").val(),
+        userTags: $("#userTags").val(),
+        userComment: $("#userComment").val()
+        },
+        success: function (result) {
+          console.log(result);
+        }   
+  	  }); //end of POST ajax
+      displayCommentToPage();
+    });
+      //displayCommentToPage();
+
+
+
+  // Gets users comment from the server and displays it to webpage
+  function displayCommentToPage() {
+    $.ajax({
+        type: "GET",
+        url: "/backliftapp/userCommentBelow",
+        success: function(result) {
+          var commentDisplay = "";
+            for(var i = 0; i < result.length; i++) {
+              commentDisplay += "<div> User: " + result[i]["userName"] + "<br /> " + result[i]["userComment"] + "<br /></div>"; 
+            }
+          $("#displayUserComment").html(commentDisplay);
+            console.dir(result);
+        }
+      }); // end of GET ajax
+  }; // end of displayCommentToPage function
+
+
+  // ajax DELETE user comments
   $.ajax({
-    type: "POST",
-    url: "/backliftapp/userCommentBelow",
-    data: {
-    userName: $("#userName").val(),
-    userTags: $("#userTags").val(),
-    userComment: $("#userComment").val()
-    },
-    success: function (result) {
-      console.log(result);
-    }
-  	}); //end of POST ajax
-    displayCommentToPage();
+	    type: "DELETE",
+	    url: "/backliftapp/userCommentBelow/82e9101d-8cbf-4b69-b61f-693d7e1c0f75",
+	    success: function(result) {
+		    console.log("Deleted!");
+	    }
+  }); // end of ajax
+
+
+  // date picker
+  $('#dp1').datepicker({
+    format: 'mm-dd-yyyy'  
   });
-  displayCommentToPage();
-});
-
-function displayCommentToPage() {
-  $.ajax({
-  type: "GET",
-  url: "/backliftapp/userCommentBelow",
-  success: function(result) {
-    var commentDisplay = "";
-    for(var i = 0; i < result.length; i++) {
-      commentDisplay += "<div> User: " + result[i]["userName"] + "<br /> " + result[i]["userComment"] + "<br /></div>"; 
-    }
-    $("#displayUserComment").html(commentDisplay);
-  }
-}); // end of GET ajax
-}; // end of displayCommentToPage function
 
 
+  // form validation
+  $(document).ready(function() {
+    $("#userSignUpForm").submit(function(e) {
+      e.preventDefault();
+    }).validate({
+      rules: {
+        firstName: {
+          minlength: 1,
+          maxlength: 30,
+          required: true
+        },
+        lastName: {
+          minlength: 1,
+          maxlength: 40,
+          required: true
+        },
+        attendSchool: {
+
+        }
+      }
+    })
+  }); // end of form validation function  
 
 
-//$.ajax({
-//	type: "DELETE",
-//	url: "/backliftapp/somecomment/",
-//	success: function(result) {
-//		console.log("Deleted!");
-//	}
-//}); // end of ajax
+ // auto complete for searching schools
+ $(function() {
+  var availableTags = [
+     "Belmont University",
+     "University of Dayton",
+     "Auburn University",
+     "Harvard University",
+     "Ohio State"
+  ];
+  $("#tags").autocomplete({
+    source: availableTags
+  });
+ }); // end of autocomplete function
+
+
+
+
+}); // end of document ready and JavaScript
 
