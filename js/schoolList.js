688aba315d315af612872715f89dd76c4e3175d4
Michelle Hey <michellemhey@gmail.com>
2013-03-17 18:54:37 -0500

    Third Commit

--- js/schoolList.js
+++ js/schoolList.js
@@ -1,15 +1,42 @@
 $(document).ready(function() {
 
-	var schoolStartA = "<p><a href='../index.html'>Auburn University</a><br/><a href='#'>Alabama State University</a></p>";
-  	
-  	var schoolStartB = "<p><a href='#'>Belmont University</a><br/></p>";
+	var schoolStart = {};
+
+	for(var key = 65; key <= 90; key++){
+		schoolStart[String.fromCharCode(key)] = "<p>";
+	}
+
+	var schoolMap = getSchoolMap();
+	var schoolList = Object.keys(schoolMap);
+	$.each(schoolList, function(i, val) {
+			schoolStart[val[0].toUpperCase()] += 
+				"<a href='" + schoolMap[val] + "'>" + val + "</a><br/>";
+	});
+
+	for(var key = 65; key <= 90; key++){
+		schoolStart[String.fromCharCode(key)] += "</p>";
 
 
-	$("#schoolsStartWithA").click(function() {
-		$("#displaySchoolPicked").html(schoolStartA);
-	});
-	$("#schoolsStartWithB").click(function() {
-		$("#displaySchoolPicked").html(schoolStartB);
-	});
+		console.log(String.fromCharCode(key));
+		console.log(schoolStart[String.fromCharCode(key)]);
+
+		$("#displaySchoolPicked").html(
+			$("#displaySchoolPicked").html() +schoolStart[String.fromCharCode(key)]
+		);
+	}	
 });
 
+function loadSchoolDataByFirstLetter(firstLetter) {
+
+	var output = "<p>";
+	var schoolMap = getSchoolMap();
+	var schoolList = Object.keys(schoolMap);
+	$.each(schoolList, function(i, val) {
+		if(val[0].toUpperCase() === firstLetter)
+			output += "<a href='" + schoolMap[val] + "'>" + val + "</a><br/>";
+	});
+
+	output += "</p>";
+
+	$("#displaySchoolPicked").html(output);
+}
