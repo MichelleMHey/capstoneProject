2bee6d891cf371fbe097825ac7de01ba43cb4952
Michelle Hey <michellemhey@gmail.com>
2013-03-17 18:55:18 -0500

    First Commit

---
+++ js/schools.js
@@ -0,0 +1,9 @@
+function getSchoolMap() {
+   var schoolMap = {};
+
+   schoolMap["Belmont University"] = "demoCommentPage.html";
+   schoolMap["University of Dayton"] = "demoCommentPage.html";
+   schoolMap["Auburn University"] = "demoCommentPage.html";
+
+  return schoolMap;
+}
