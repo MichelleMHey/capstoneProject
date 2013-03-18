4e7994094b6a7bbf538e8cdad3dfa6e725f561e5
Michelle Hey <michellemhey@gmail.com>
2013-03-18 11:46:43 -0500

    Second Commit

--- js/schools.js
+++ js/schools.js
@@ -1,9 +1,35 @@
 function getSchoolMap() {
    var schoolMap = {};
 
-   schoolMap["Belmont University"] = "demoCommentPage.html";
-   schoolMap["University of Dayton"] = "demoCommentPage.html";
-   schoolMap["Auburn University"] = "demoCommentPage.html";
+   schoolMap["Belmont University"] = "../schoolPages/belmont.html";
+   schoolMap["University of Dayton"] = "../schoolPages/univDayton.html";
+   schoolMap["Auburn University"] = "../schoolPages/auburnUniv.html";
+   schoolMap["Colorado State University"] = "../schoolPages/coloradoStateUniv.html";
+   schoolMap["Duke University"] = "../schoolPages/dukeUniv.html";
+   schoolMap["Emory University"] = "../schoolPages/emoryUniv.html";
+   schoolMap["Florida State University"] = "../schoolPages/floridaStUniv.html";
+   schoolMap["Georgetown University"] = "../schoolPages/georgetownUniv.html";
+   schoolMap["Indiana State University"] = "../schoolPages/indianaStUniv.html";
+   schoolMap["Johns Hopkins University"] = "../schoolPages/johnsHopkinsUniv.html";
+   schoolMap["Louisiana State University"] = "../schoolPages/louisianaStUniv.html";
+   schoolMap["New York University"] = "../schoolPages/newYorkUniv.html";
+   schoolMap["Pennsylvanis State University"] = "../schoolPages/pennStUniv.html";
+   schoolMap["Saint Johns University"] = "../schoolPages/stJohnsUniv.html";
+   schoolMap["Baylor University"] = "../schoolPages/baylorUniv.html";
+   schoolMap["Cazenovia College"] = "../schoolPages/cazenoviaCollege.html";
+   schoolMap["Harvard University"] = "../schoolPages/harvardUniv.html";
+   schoolMap["Kent State University"] = "../schoolPages/kentStUniv.html";
+   schoolMap["Miami University (OH)"] = "../schoolPages/miamiUnivOH.html";
+   schoolMap["Oklahoma State University"] = "../schoolPages/oklahomaStUniv.html";
+   schoolMap["Quincy University"] = "../schoolPages/quincyUniv.html";
+   schoolMap["Rice University"] = "../schoolPages/riceUniv.html";
+   schoolMap["Tennessee State University"] = "../schoolPages/tennStUniv.html";
+   schoolMap["University of Chicago"] = "../schoolPages/univChicago.html";
+   schoolMap["University of Kentucky"] = "../schoolPages/univKentucky.html";
+   schoolMap["Vanderbilt University"] = "../schoolPages/vanderbiltUniv.html";
+   schoolMap["Washington State University"] = "../schoolPages/washingtonStUniv.html";
+   schoolMap["Xavier University"] = "../schoolPages/xavierUniv.html";
+   schoolMap["Yale University"] = "../schoolPages/yaleUniv.html";
 
   return schoolMap;
-}
+};
